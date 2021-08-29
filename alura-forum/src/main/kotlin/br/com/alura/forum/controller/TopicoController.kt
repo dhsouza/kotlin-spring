bd23fcdff593536d8ea0ff9e298c8c4f3536aafa
Danilo Hernandes de Souza <danilohernandesdesouza@gmail.com>
2021-08-29 19:54:46 -0300

    Bean validation

--- alura-forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ alura-forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -4,6 +4,7 @@ import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/topicos")
@@ -22,7 +23,7 @@ class TopicoController(
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody topico: NovoTopicoForm) {
+    fun cadastrar(@RequestBody @Valid topico: NovoTopicoForm) {
         topicoService.cadastrar(topico)
     }
 }
